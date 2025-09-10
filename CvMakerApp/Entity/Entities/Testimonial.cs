6dc8bfd1dc965f4eaca9e10774056e930c2d3de2
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-09-10 13:59:19 +0300

    Frontend dosyaları eklendi, Code-First yaklaşımı ile veritabanı oluşturuldu.

---
+++ CvMakerApp/Entity/Entities/Testimonial.cs
@@ -0,0 +1,10 @@
+﻿namespace CvMakerApp.Entity.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string TestimonialName { get; set; }
+        public string Position { get; set; }
+        public string Company { get; set; }
+    }
+}
