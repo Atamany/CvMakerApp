d90e1470144b00e752baa791891074599fdc12c3
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-09-10 17:18:43 +0300

    Admin Frontend tamamlandı!

--- CvMakerApp/Controllers/DefaultController.cs
+++ CvMakerApp/Controllers/DefaultController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CvMakerApp.Entity.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CvMakerApp.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
