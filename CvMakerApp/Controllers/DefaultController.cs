ce5f15171a309666db3e3bdf1063ba6ad1e7642e
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-09-15 12:51:45 +0300

    Admin panelinde Title ve Description menüleri tamamlandı!

--- CvMakerApp/Controllers/DefaultController.cs
+++ CvMakerApp/Controllers/DefaultController.cs
@@ -14,7 +14,72 @@ namespace CvMakerApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            var deger = _context.Titles.FirstOrDefault();
+            return View(deger);
+        }
+        [HttpPost]
+        public IActionResult Index(Entity.Entities.Title title)
+        {
+            var deger = _context.Titles.FirstOrDefault();
+            if (deger != null)
+            {
+                deger.Name = title.Name;
+                deger.Surname = title.Surname;
+                deger.Description = title.Description;
+                deger.WebUrl = title.WebUrl;
+                deger.Phone = title.Phone;
+                deger.Email = title.Email;
+                deger.ImageUrl = title.ImageUrl;
+                _context.Update(deger);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index", "Default");
+        }
+        [HttpGet]
+        public IActionResult Description()
+        {
+            var deger = _context.Descriptions.ToList();
+            return View(deger);
+        }
+        [HttpGet]
+        public IActionResult AddDescription()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDescription(Entity.Entities.Description description)
+        {
+            _context.Add(description);
+            _context.SaveChanges();
+            return RedirectToAction("Description");
+        }
+        public IActionResult DeleteDescription(int id)
+        {
+            var deger = _context.Descriptions.Find(id);
+            if (deger != null)
+            {
+                _context.Descriptions.Remove(deger);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Description");
+        }
+        [HttpGet]
+        public IActionResult UpdateDescription(int id)
+        {
+            var deger = _context.Descriptions.Find(id);
+            return View(deger);
+        }
+        [HttpPost]
+        public IActionResult UpdateDescription(Entity.Entities.Description description)
+        {
+            var deger = _context.Descriptions.Find(description.DescriptionId);
+            if (deger != null)
+            {
+                deger.DescriptionText = description.DescriptionText;
+                _context.Update(deger);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Description", "Default");
         }
     }
 }
